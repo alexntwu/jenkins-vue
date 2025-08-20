96627905e8fa6eb2c92253a45d01b69693b341be
Alex N.T. Wu <alexnt.wu@gmail.com>
2025-08-20 22:14:37 +0800

    Add files via upload
    202550820

---
+++ vue-helloworld/README.md
@@ -0,0 +1,29 @@
+# vue-helloworld
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+## Project preview 自行手動加入的指令
+```
+npx vite preview --port 4173 --host 
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-helloworld/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.hello {
+	text-align: center;
+	box-shadow: 3px 1px 18px 3px rgba(0,0,0,0.19);
+	font-weight: 400;
+	font-size: 600%;
+	margin-left: 350px;
+	min-height: 190px;
+	width: 800px;
+	padding-top: 8%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
